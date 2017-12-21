ac2e651fe39f51912fd1e0e36008e460a55e6847
Grzegorz Burzynski <czeslavo@gmail.com>
2017-12-21 13:25:34 +0100

    fix boost url

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -3,9 +3,9 @@
 mkdir tmp 
 pushd tmp
 
-wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1.66.0.tar.gz
-tar -xf boost_1.66.0.tar.gz 
-pushd boost_1.66.0
+wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
+tar -xf boost_1_66_0.tar.gz 
+pushd boost_1_66_0
 echo "- Installing boost..."
 ./bootstrap.sh --with-libraries=context,coroutine,fiber,program_options,system --prefix=${TRAVIS_BUILD_DIR}/boost &> /dev/null
 ./b2 install &> /dev/null
