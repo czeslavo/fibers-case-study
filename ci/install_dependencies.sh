36ec6c01121107ba2dfcd099f940a15360aceda2
Grzegorz Burzynski <czeslavo@gmail.com>
2017-12-21 11:34:23 +0100

    reduce verbosity

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -4,9 +4,9 @@ mkdir tmp
 pushd tmp
 
 wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
-tar -xvf boost_1_66_0.tar.gz 
+tar -xf boost_1_66_0.tar.gz 
 pushd boost_1_66_0
-./bootstrap.sh --with-libraries=context,coroutine,fiber,program_options,system --prefix=${TRAVIS_BUILD_DIR}/boost
-./b2 install
-
-
+echo "- Installing boost..."
+./bootstrap.sh --with-libraries=context,coroutine,fiber,program_options,system --prefix=${TRAVIS_BUILD_DIR}/boost &> /dev/null
+./b2 install &> /dev/null
+echo "- Boost installed"
