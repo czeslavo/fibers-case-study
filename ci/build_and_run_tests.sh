92b9c60d51767a576d74ff02c6df3b6104bd9467
Grzegorz Burzynski <czeslavo@gmail.com>
2017-12-21 13:13:14 +0100

    add boost includedir as global in cmake

--- ci/build_and_run_tests.sh
+++ ci/build_and_run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+BOOST_DIR=${TRAVIS_BUILD_DIR}/boost
+BOOST_CMAKE_FLAGS="-DBOOST_ROOT=${BOOST_DIR} -DBOOST_INCLUDEDIR=${BOOST_DIR}/include -DBOOST_LIBRARYDIR=${BOOST_DIR}/lib" 
+
 cd code && mkdir -p build && cd build
-cmake .. -DUSE_ALL=1 -DWITH_TESTS=1 -DBOOST_ROOT=${TRAVIS_BUILD_DIR}/boost
+cmake .. -DUSE_ALL=1 -DWITH_TESTS=1 "${BOOST_CMAKE_FLAGS}"
 make -j $(nproc)
-ctest --vebose --color 
+ctest --vebose --color
