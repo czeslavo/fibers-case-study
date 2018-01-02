3b8c37acd95a8a28388dd79c28b99d4a6bc953e4
Grzegorz Burzynski <czeslavo@gmail.com>
2018-01-02 21:23:03 +0100

    clean

--- ci/build_and_run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BOOST_DIR="${TRAVIS_BUILD_DIR}/boost"
-BOOST_CMAKE_FLAGS="-DBOOST_ROOT=${BOOST_DIR}" 
-
-cd code && mkdir -p build && cd build
-cmake .. -DUSE_ALL=1 -DWITH_TESTS=1 "${BOOST_CMAKE_FLAGS}"
-make -j
---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd code && mkdir -p build && cd build
+cmake .. -DUSE_ALL=1
+make -j
